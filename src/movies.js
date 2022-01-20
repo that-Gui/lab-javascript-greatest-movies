9feae4e177c3a129afa183be8d8d91d4b1fff055
thatGui <gui@thatgui.com>
2022-01-20 17:58:16 +0000

    initial commit

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,26 @@
+const movies = require('./data.js');
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors( movies ) {
+  const directors = movies.map( ele => ele.director );
+  
+  return directors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies( movies ) {
+  const speil = movies.filter( ele => ele.genre.includes('Drama') && ele.director === 'Steven Spielberg');
+  return speil.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage( movies ) {
+  if ( !movies.length ) return 0;
+  const totalScore = movies.reduce((accumulator, currentValue) => accumulator + currentValue.score );
+ return Math.round((totalScore / movies.length) * 100) / 100;
+ 
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
